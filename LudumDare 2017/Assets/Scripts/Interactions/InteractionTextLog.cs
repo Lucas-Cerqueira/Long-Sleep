40334a158e0f2419d311358b5c1b0cbdea78b25d
LucasCerqueira <lucas_de.andrade@hotmail.com>
2017-04-22 16:10:01 -0300

    Added TextLog interaction and UI text

---
+++ LudumDare 2017/Assets/Scripts/Interactions/InteractionTextLog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionTextLog : InteractionGeneric {
+
+	private TextLogUIHandler textLogUIHandler;
+
+	// Use this for initialization
+	void Start () 
+	{
+		textLogUIHandler = GameObject.Find ("TextLogUI").GetComponent<TextLogUIHandler>();
+	}
+		
+
+	public override void Interaction ()
+	{
+		textLogUIHandler.Enable ();
+	}
+}
---
+++ LudumDare 2017/Assets/Scripts/TextLog/TextLogUIHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLogUIHandler : MonoBehaviour {
+
+	private Image image;
+	private GameObject text;
+	private bool enabled = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		image = GetComponent<Image> ();
+		text = transform.GetChild (0).gameObject;
+	}
+	
+	public void Enable ()
+	{
+		image.enabled = true;
+		text.SetActive (true);
+		enabled = true;
+	}
+
+	public void Disable ()
+	{
+		image.enabled = false;
+		text.SetActive (false);
+		enabled = false;
+	}
+
+	void Update ()
+	{
+		if (enabled && (Input.GetKeyDown (KeyCode.Escape) || Input.GetMouseButtonDown (0)))
+			Disable ();
+			
+	}
+}
