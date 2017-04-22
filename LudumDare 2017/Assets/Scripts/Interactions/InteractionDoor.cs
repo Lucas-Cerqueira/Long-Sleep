b8049d6da1266e6aba70f40ff993a6681a7cacd4
Diogo <diogo1243@gmail.com>
2017-04-22 14:58:25 -0300

    Interactions changed.

---
+++ LudumDare 2017/Assets/Scripts/Interactions/InteractionDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionDoor : InteractionGeneric {
+	public bool isLocked = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void Interaction ()
+	{
+		if (!isLocked) {
+			for (int i = 0; i < transform.childCount; i++) {
+				transform.GetChild (i).GetComponent<OpenDoor> ().OpenDoors ();
+			}
+			transform.GetComponent<Collider> ().enabled = false;
+		}
+	}
+
+	public void Unlock ()
+	{
+		isLocked = false;
+	}
+}
