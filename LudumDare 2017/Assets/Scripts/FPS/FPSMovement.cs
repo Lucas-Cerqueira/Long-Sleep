d680ac23e322cf8b6613698d62672ad85e081967
LucasCerqueira <lucas_de.andrade@hotmail.com>
2017-04-22 14:42:40 -0300

    FPS movement and camera look scripts

---
+++ LudumDare 2017/Assets/Scripts/FPS/FPSMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class FPSMovement : MonoBehaviour
+{
+    public float characterMass = 5;
+    public float walkSpeed = 5;
+    public float runSpeed = 10;
+	public float maxVelocityChange = 1;
+	public bool canJump = true;
+	public float jumpHeight = 2f;
+	public bool airControl = true;
+
+    private Vector3 targetVelocity;
+	private Vector3 velocityChange;
+    private Transform cameraTransform;
+	private Rigidbody body;
+	private CapsuleCollider collider;
+	private float speed = 5;
+    private int layerMask = 1 << 8;
+
+    // Use this for initialization
+    void Awake()
+    {
+		cameraTransform = Camera.main.transform;
+		body = GetComponent<Rigidbody> ();
+		body.freezeRotation = true;
+
+		collider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		if (Input.GetKey (KeyCode.LeftShift))
+			speed = runSpeed;
+		else
+			speed = walkSpeed;
+
+		RaycastHit hitinfo;
+		bool grounded = Physics.Raycast (transform.position, Vector3.down, collider.height/2f + 0.1f);
+
+		if ((grounded)|| (!grounded && airControl))
+		{
+			targetVelocity = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			targetVelocity = cameraTransform.TransformDirection (targetVelocity);
+			targetVelocity *= speed;
+
+			velocityChange = targetVelocity - body.velocity;
+
+			velocityChange.x = Mathf.Clamp (velocityChange.x, -maxVelocityChange, maxVelocityChange);
+			velocityChange.z = Mathf.Clamp (velocityChange.z, -maxVelocityChange, maxVelocityChange);
+			velocityChange.y = 0;
+
+			body.AddForce (velocityChange, ForceMode.VelocityChange);
+		}
+
+		if (grounded && canJump) 
+		{
+			if (Input.GetButtonDown ("Jump"))
+			{
+				body.AddForce (transform.up * jumpHeight);
+				grounded = false;
+			}
+		}
+		else 
+		{
+			body.AddForce (Physics.gravity * body.mass);
+		}        
+    }
+}
---
+++ LudumDare 2017/Assets/Scripts/FPS/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public float sensibilityX = 2;
+	public float sensibilityY = 2;
+	public float verticalClamp = 80f;
+
+	private float rotationX = 0f;
+	private float rotationY = 0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rotationX += Input.GetAxis ("Mouse X") * sensibilityX;
+		rotationY += -Input.GetAxis ("Mouse Y") * sensibilityY;
+		rotationY = Mathf.Clamp (rotationY, -verticalClamp, verticalClamp);
+
+		transform.localEulerAngles = new Vector3 (rotationY, rotationX, 0);
+	}
+}
