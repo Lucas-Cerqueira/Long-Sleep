ad7df66bab18d5454b3da6543cee08599c124f5a
Diogo <diogo1243@gmail.com>
2017-04-22 16:45:37 -0300

    LimitTime done!

---
+++ LudumDare 2017/Assets/Scripts/LimitTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitTime : MonoBehaviour {
+	public float limitTime = 600.0f;
+	private float startTime;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTime > limitTime)
+			print ("Acabou o tempo");
+	}
+}
