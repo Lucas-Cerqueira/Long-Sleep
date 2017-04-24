96561c31f27020a25a2c26410dc1010690df5ff8
Diogo <diogo1243@gmail.com>
2017-04-24 17:51:08 -0300

    Changed Dialogues and Logs text.

--- LudumDare 2017/Assets/Scripts/Dialogue/DialogueText.cs
+++ LudumDare 2017/Assets/Scripts/Dialogue/DialogueText.cs
@@ -6,17 +6,17 @@ public class DialogueText : MonoBehaviour {
 
 	public static Dictionary<string, string[]> dialoguesList = new Dictionary<string, string[]> {
 		{ "wakeUp", new string[]{ "Shit...cryosleeping always makes my muscle hurts..", "Where's the doctor? Wasn't he supposed to be here at all times?"} },
-		{ "medic", new string[]{ "How much did I sleep? This doesn't make sense."} },
+		{ "medic", new string[]{ "How much did I sleep? It makes no sense."} },
 		{ "security", new string[]{ "Yeah...bureaucracy sucks, welcome to the 31st century, pal."}},
 		{ "atLounge", new string[]{ "Holy shit! I have to get out of here."}},
 		{ "electrician", new string[]{ "Damn, I have to fix this fast."}},
 		{ "atCoffee", new string[]{ "Where is everybody? What the hell happened here?"}},
 		{ "atBasement", new string[]{ "We should have way more supplies than this...how many years have I slept?"}},
-		{ "afterFixingFuse", new string[]{ "Alright, we still have time."}},
-		{ "atCaptain", new string[]{ "Wow, look at this view.\nHome...it looks so small from here."}},
-		{ "afterPickingCard", new string[]{ "Ok, let's finish this."}},
+		{ "afterFixingFuse", new string[]{ "Alright, we've got some time."}},
+		{ "atCaptain", new string[]{ "Wow, look at this view.\nHome...it looks so small from here. haha"}},
+		{ "afterPickingCard", new string[]{ "Cool, let's get this over with."}},
 		{ "afterOpeningEscapePodRoom", new string[]{ "Finally!"}},
-		{ "insideEscapePod", new string[]{ "Home...so distant...so small..."}}
+		{ "insideEscapePod", new string[]{ "Home...so far...so small..."}}
 
 	};
 
